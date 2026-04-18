6b5f4653d1ae258f1d0d1551fd09578a149cfcb1
K <da2d9rwh@duck.com>
2026-04-17 21:09:38 -0700

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# What and Why 
+Whenever making plans, I find myself leaning toward unix cal
+
+Similar to how cal can be used to print a calendar, I wanted a way to print (Date, Weekday) pairs whenever composing plans
+
+iCal prints a new-line by default mapping of weekday abbreviations to day numbers for a month
+
+# Example output
+python ical.py 5 2026
+
+Fri 1
+Sat 2
+Sun 3
+Mon 4
+Tue 5
+Wed 6
+...
---
+++ ical.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Print a separated (new-line by default) mapping of weekday abbreviations to day numbers for a month.
+# Example output for April 2026: "Wed 1, Thu 2, Fri 3, Sat 4, Sun 5, Mon 6, ..."
+
+from datetime import date
+import calendar
+import sys
+
+
+def month_weekday_map(month, year):
+    days_in_month = calendar.monthrange(year, month)[1]
+
+    names = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    raw_mappings = []
+
+    for day_in_month in range(1, days_in_month + 1):
+        datetime_in_month = date(year, month, day_in_month)
+        raw_mappings.append(f"{names[datetime_in_month.weekday()]} {day_in_month}")
+    print(raw_mappings)
+    print("\n".join(raw_mappings))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        try:
+            m = int(sys.argv[1])
+            y = int(sys.argv[2])
+
+            if len(str(y)) == 2:
+                y = int("20" + str(y))
+                print(f"Normalized year to {y} from {(sys.argv[2])}")
+
+            month_weekday_map(m, y)
+        except ValueError:
+            print("Usage: script.py [month] [year]")
